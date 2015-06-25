98e5378073237a6ddb6e77eb01339fe3ff709499
martijn13795 <martijn13795@hotmail.com>
2015-06-25 21:05:39 +0200

    icons

--- index.php
+++ index.php
@@ -11,25 +11,25 @@
             <div class="col-xs-12 col-md-12"><h1>Welkom bij <strong>EKC 2000</strong></h1><hr></div>
             <div class="row">
                 <div class="col-xs-12 col-md-12">
-                    <div class="col-xs-12 col-md-4 well">
-                        <div class="fa-center"><i class="fa fa-newspaper-o fa-5x"></i></div>
-                        </br><p>Nieuws07-03-15 Programma EKC 2000-dag</br></br>
+                    <div class="col-xs-12 col-md-4 well infoDiv">
+                        <div class="fa-center"><i class="icon major fa-newspaper-o"></i></div>
+                        <p>Nieuws07-03-15 Programma EKC 2000-dag</br></br>
                             Nieuws07-03-15 EKC 2000 B2 is zaalkampioen 2014-2015!</br></br>
                             Nieuws07-03-15 Paasspelen 2015!</br></br>
                             Nieuws22-02-15 Word donateur en steun EKC 2000 op financieel gebied!</br></br>
                             Nieuws16-02-15 Interview met Riejander Deen</p>
                     </div>
-                    <div class="col-xs-12 col-md-4 well">
-                        <div class="fa-center"><i class="fa fa-camera fa-5x"></i></div>
-                        </br><p>Fotoalbum17-05-14 EKC 2000 1 - Elko 1</br></br>
+                    <div class="col-xs-12 col-md-4 well infoDiv">
+                        <div class="fa-center"><i class="icon major fa-camera"></i></div>
+                        <p>Fotoalbum17-05-14 EKC 2000 1 - Elko 1</br></br>
                             Fotoalbum17-05-14 EKC 2000 2 - Elko 2</br></br>
                             Fotoalbum10-05-14 Flamingo's 1 - EKC 2000 1</br></br>
                             Fotoalbum19-04-14 EKC 2000 1 - Pallas'08 1</br></br>
                             Fotoalbum19-04-14 EKC 2000 2 - Pallas'08 2</p>
                     </div>
-                    <div class="col-xs-12 col-md-4 well">
-                        <div class="fa-center"><i class="fa fa-users fa-5x"></i></div>
-                        </br><p>Activiteit12-03-15 EKC 2000-dag</p>
+                    <div class="col-xs-12 col-md-4 well infoDiv1">
+                        <div class="fa-center"><i class="icon major fa-users"></i></div>
+                        <p>Activiteit12-03-15 EKC 2000-dag</p>
                     </div>
                 </div>
             </div>
