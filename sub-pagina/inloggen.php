1ba8a07654b3df3b13121aecd27f6522cb7b8077
martijn13795 <martijn13795@hotmail.com>
2015-07-05 21:53:24 +0200

    button collor

--- sub-pagina/inloggen.php
+++ sub-pagina/inloggen.php
@@ -15,7 +15,7 @@
                 <form method="POST" action="../sub-pagina/inloggen.php" accept-charset="UTF-8">
                     <input type="text" id="username" class="form-control" name="username" placeholder="Gebruikersnaam"><br />
                     <input type="password" id="password" class="form-control" name="password" placeholder="Wachtwoord"><br />
-                    <button type="submit" name="submit" class="btn btn-blue btn-block">Inloggen</button>
+                    <button type="submit" name="submit" class="btn btn-primary btn-block">Inloggen</button>
                 </form>
             </div>
         </div>
