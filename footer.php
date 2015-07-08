a42b4341425d42a8094d2c90be8fdc842e96a5bd
martijn13795 <martijn13795@hotmail.com>
2015-07-08 19:55:37 +0200

    new footer?

--- footer.php
+++ footer.php
@@ -1,5 +1,14 @@
 <footer class="footer">
     <div class="container">
-        <p class="text-muted">EKC 2000</p>
+        <a href="https://facebook.com/EKC2000" target="_blank">
+            <i class="fa fa-facebook"></i>
+        </a>
+        <a href="https://twitter.com/EKC2000_Emmen" target="_blank">
+            <i class="fa fa-twitter"></i>
+        </a>
+        <a href="https://nl.linkedin.com/in/martijnposthumainf" target="_blank" onClick="window.open('https://nl.linkedin.com/in/casvandinter')">
+            <i class="fa fa-linkedin"></i>
+        </a></br>
+        &copy; EKC 2000
     </div>
 </footer>
