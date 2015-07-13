a75434dfd39ccd53e4b95916a1688451c0109399
martijn13795 <martijn13795@hotmail.com>
2015-07-14 00:24:29 +0200

    database verbinding

--- sub-pagina/activiteiten.php
+++ sub-pagina/activiteiten.php
@@ -1,17 +1,4 @@
 <?php include '../includes/html.php';?>
     <div class="container">
-        <?php
-        $servername = "192.168.1.145";
-        $username = "ekc2000";
-        $password = "xH2b8C5PnajhnXJ5";
 
-        // Create connection
-        $conn = new mysqli($servername, $username, $password);
-
-        // Check connection
-        if ($conn->connect_error) {
-            die("Connection failed: " . $conn->connect_error);
-        }
-        echo "Connected successfully";
-        ?>
     </div>
