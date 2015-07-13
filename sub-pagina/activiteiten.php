1ebe2bec39559a1b0852f67a2588b0cbff1759e6
martijn13795 <martijn13795@hotmail.com>
2015-07-13 23:48:58 +0200

    test

--- sub-pagina/activiteiten.php
+++ sub-pagina/activiteiten.php
@@ -1,4 +1,17 @@
 <?php include '../includes/html.php';?>
     <div class="container">
+        <?php
+        $servername = "ekc2000.party";
+        $username = "ekc2000";
+        $password = "xH2b8C5PnajhnXJ5";
 
+        // Create connection
+        $conn = new mysqli($servername, $username, $password);
+
+        // Check connection
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+        echo "Connected successfully";
+        ?>
     </div>
