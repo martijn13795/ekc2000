0de4f691710b92cd14dc2bd17844ba01eed6cfc8
martijn13795 <martijn13795@hotmail.com>
2015-07-15 15:01:10 +0200

    clear up

--- js/chat_script.js
+++ js/chat_script.js
@@ -1,3 +1,7 @@
+//Twitter functie
+!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
+
+//Chat functie
 $(document).ready(function() {
     $("#sub").click(function () {
         $.post($("#chat").attr("action"),
@@ -9,10 +13,12 @@ $(document).ready(function() {
     });
 });
 
+//Blijf op pagina na submit
 $("#chat").submit( function() {
     return false;
 });
 
+//Blijf op pagina en submit bij enter toets
 $(document).ready(function(){
     $("input").keypress(function (e) {
         var k = e.keyCode || e.which;
@@ -29,18 +35,21 @@ $(document).ready(function(){
     });
 });
 
+//clear input na submit
 function clearInput() {
     $("#chat :input").each( function() {
         $(this).val('');
     });
 }
 
+//Refresh function
 $(document).ready(function() {
     setInterval(function chatRefresh() {
         $('#chatRefresh').load('/includes/chatRefresh.php')
     }, 5000);
 });
 
+//Omlaag scroll functtie
 $(document).ready(function() {
     function chat() {
         var ActualscrollHeight = document.getElementById("panel-body").clientHeight;
