dda8b5af03cea9740585fc167525f5487f8163be
martijn13795 <martijn13795@hotmail.com>
2015-07-16 18:42:43 +0200

    tijdelijk

--- sub-pagina/commissies.php
+++ sub-pagina/commissies.php
@@ -1,4 +1,78 @@
 <?php include '../includes/html.php';?>
     <div class="container">
-
+        <div class="col-md-12 col-xs-12">
+            <h1>De commissies van EKC 2000</h1><hr>
+            <div class="col-md-5 col-xs-12 well commissieDiv">
+                <h3>Technische commissie:</h3>
+                <p>Petra Bethlehem</br>
+                    Ilse Schuttrups</br>
+                    Karin Timmerman</br>
+                    Alex Veenstra</br>
+                    Erwin Kort</br>
+                    Gert Pruim</p>
+            </div>
+            <div class="col-md-5 col-md-offset-2 col-xs-12 well commissieDiv">
+                <h3>PR- & Communicatie-commissie:</h3>
+                <p>Jan Westerhof</br>
+                    Redmer Bouma</br>
+                    David Sleeking</br>
+                    Bert Schepers</p>
+            </div>
+            <div class="col-md-5 col-xs-12 well commissieDiv">
+                <h3>Sponsorcommissie:</h3>
+                <p>Vacant</br>
+                    Erik Post</br>
+                    Jelmer Bouma</p>
+            </div>
+            <div class="col-md-5 col-md-offset-2 col-xs-12 well commissieDiv">
+                <h3>Jeugdactiviteitencommissie:</h3>
+                <p>Jannetta Deen</br>
+                    Gerda Pol</br>
+                    Maaike Smits</br>
+                    Patrick Posthuma</br>
+                    Ralph van Straten</br>
+                    Margot Dol</p>
+            </div>
+            <div class="col-md-5 col-xs-12 well commissieDiv">
+                <h3>Kantinecommissie:</h3>
+                <p>Alex Veenstra</br>
+                    Theo Venema</br>
+                    Ina Venema</br>
+                </p>
+            </div>
+            <div class="col-md-5 col-md-offset-2 col-xs-12 well commissieDiv">
+                <h3>Ledenadministratie:</h3>
+                <p>Jolanda ten Broeke</p>
+            </div>
+            <div class="col-md-5 col-xs-12 well commissieDiv">
+                <h3>Kledingcommissie:</h3>
+                <p>Lizeth Timmerman</br>
+                    Bianca Heeres</p>
+            </div>
+            <div class="col-md-5 col-md-offset-2 col-xs-12 well commissieDiv">
+                <h3>Materiaalbeheer:</h3>
+                <p>Theo Venema</br>
+                    Harry Deen</p>
+            </div>
+            <div class="col-md-5 col-xs-12 well commissieDiv">
+                <h3>Schoolkorfbalcommissie:</h3>
+                <p>Petra Bethlehem</br>
+                    Harma Frans</br>
+                    Monique Hekhuis</br>
+                    Marloes Heijblom</p>
+            </div>
+            <div class="col-md-5 col-md-offset-2 col-xs-12 well commissieDiv">
+                <h3>Halcommissie:</h3>
+                <p>Froukje van de Werf</p>
+            </div>
+            <div class="col-md-5 col-xs-12 well commissieDiv">
+                <h3>Scheidsrechteraanwijzer:</h3>
+                <p>Michael Beekman</p>
+            </div>
+            <div class="col-md-5 col-md-offset-2 col-xs-12 well commissieDiv">
+                <h3>Wedstrijdsecretariaat Senioren/Jeugd:</h3>
+                <p>Petra Bethlehem</br>
+                    Ilse Schutrups</p>
+            </div>
+        </div>
     </div>
