974f4c385327e71ca66ea715b349ac501d165e14
martijn13795 <martijn13795@hotmail.com>
2015-07-16 15:36:26 +0200

    bug fix in hover

--- js/my_script.js
+++ js/my_script.js
@@ -1,6 +1,6 @@
 //menu on mouse over functie
 $(document).ready(function () {
-    $('.closed').hover(function () {
+    $('.closed, .active').hover(function () {
         $(this).addClass('open');
     }, function () {
         $(this).removeClass('open');
