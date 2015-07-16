f28703f0b05900666b681a8e35c01d3bcdb6aa7a
martijn13795 <martijn13795@hotmail.com>
2015-07-16 15:37:43 +0200

    link fix

--- sub-pagina/inloggen.php
+++ sub-pagina/inloggen.php
@@ -4,7 +4,7 @@
             <div class="col-md-4 col-md-offset-4 well">
                 <legend>Inloggen</legend>
                 <div class="form-group">
-                    <form method="POST" action="../sub-pagina/inloggen.php" accept-charset="UTF-8">
+                    <form method="POST" action="/inloggen" accept-charset="UTF-8">
                         <input type="text" id="username" class="form-control" name="username" placeholder="Gebruikersnaam"><br />
                         <input type="password" id="password" class="form-control" name="password" placeholder="Wachtwoord"><br />
                         <button type="submit" name="submit" class="btn btn-primary btn-block">Inloggen</button>
