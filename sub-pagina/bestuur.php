e7d37257b0b1b4c97258b0c4f4e9b8e498db5105
martijn13795 <martijn13795@hotmail.com>
2015-07-16 18:17:39 +0200

    bestuur en over-de-club

--- sub-pagina/bestuur.php
+++ sub-pagina/bestuur.php
@@ -1,4 +1,48 @@
 <?php include '../includes/html.php';?>
     <div class="container">
-
+        <div class="col-md-12 col-xs-12">
+            <h1>Het bestuur van EKC 2000</h1><hr>
+            <div class="row">
+                <div class="col-md-5 col-xs-12 well bestuurDiv">
+                    <p>
+                    <img src="../images/bestuur/wim.jpg" class="img-responsive bestuurImg" alt="Wim Bloeming"/>
+                        <h3>Wim Bloeming</h3>
+                        Functie: Sponsorzaken/ Ad interim voorzitter</br></br>
+                        Contact: 06-50422677
+                    </p>
+                </div>
+                <div class="col-md-5 col-md-offset-2 col-xs-12 well bestuurDiv">
+                    <p>
+                        <img src="../images/bestuur/patrick.jpg" class="img-responsive" alt="Wim Bloeming"/>
+                        <h3>Patrick Posthuma</h3>
+                        Functie: Penningmeester</br></br>
+                        Contact: 06-83986717
+                    </p>
+                </div>
+                <div class="col-md-5 col-xs-12 well bestuurDiv">
+                    <p>
+                        <img src="../images/bestuur/jan.jpg" class="img-responsive bestuurImg" alt="Wim Bloeming"/>
+                        <h3>Jan Westerhof</h3>
+                        Functie: PR & Communicatie</br></br>
+                        Contact: 06-31311490
+                    </p>
+                </div>
+                <div class="col-md-5 col-md-offset-2 col-xs-12 well bestuurDiv">
+                    <p>
+                        <img src="../images/bestuur/bernadette.jpg" class="img-responsive bestuurImg" alt="Wim Bloeming"/>
+                        <h3>Bernadette van Loon</h3>
+                        Functie: Secretaris</br></br>
+                        Contact: 591-630488
+                    </p>
+                </div>
+                <div class="col-md-5 col-xs-12 well bestuurDiv">
+                    <p>
+                        <img src="../images/bestuur/wietske.jpg" class="img-responsive bestuurImg" alt="Wim Bloeming"/>
+                        <h3>Wietske Heida</h3>
+                        Functie: Technische zaken</br></br>
+                        Contact: 06-48888290
+                    </p>
+                </div>
+            </div>
+        </div>
     </div>
--- sub-pagina/over-de-club.php
+++ sub-pagina/over-de-club.php
@@ -1,4 +1,7 @@
 <?php include '../includes/html.php';?>
     <div class="container">
-
+        <div class="col-md-12 col-xs-12">
+            <h1>Over de club</h1><hr>
+            <p>Binnenkort kunt je hier meer lezen over het onstaan van de vereniging.</p>
+        </div>
     </div>
