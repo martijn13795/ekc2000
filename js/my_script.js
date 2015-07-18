c5194f8e9cf79374180c8f600e3c144b5a1e9869
martijn13795 <martijn13795@hotmail.com>
2015-07-18 14:33:35 +0200

    test

--- js/my_script.js
+++ js/my_script.js
@@ -41,7 +41,7 @@ $(document).ready(function() {
             });
         }
         else {
-            $('img[data-src-320px]').each(function () {
+            $('.galerijImg, img[data-src-320px]').each(function () {
                 $(this).attr('src',$(this).attr('data-src-320px'));
             });
         }
