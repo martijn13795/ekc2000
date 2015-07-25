18bf0d09f9593ed7a339962bda4f31d5fa7b0f73
martijn13795 <martijn13795@hotmail.com>
2015-07-25 18:40:58 +0200

    extra menu hover functie

--- js/my_script.js
+++ js/my_script.js
@@ -1,5 +1,13 @@
 //menu on mouse over functie
 $(document).ready(function () {
+
+    $('ul').hover(function () {
+        $(".open > .dropdown-toggle").attr("style","color: rgba(255, 255, 255, 1); background-color: rgba(55, 192, 251, 1);");
+    }, function () {
+        $(".dropdown-toggle").attr("style","");
+    });
+
+
     $('.closed, .active').hover(function () {
         $(this).addClass('open');
     }, function () {
