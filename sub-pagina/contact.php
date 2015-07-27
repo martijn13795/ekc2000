59b0bd490468e0597a97a2272c78a30560446496
martijn13795 <martijn13795@hotmail.com>
2015-07-27 16:40:55 +0200

    mobile fix

--- sub-pagina/contact.php
+++ sub-pagina/contact.php
@@ -7,17 +7,20 @@
                     <label>Naam: </label><input type="text" class="form-control contactMargin" name="name" placeholder="Naam" REQUIRED><br>
                     <label>Email: </label><input type="email" class="form-control contactMargin" name="email" placeholder="Email" REQUIRED><br>
                     <label>Bericht: </label><textarea rows="6" class="form-control contactMargin" placeholder="Type hier uw bericht..." REQUIRED></textarea><br>
-                    <input type="submit" name="submit" value="Verstuur" class="btn btn-primary pull-right">
+                    <input type="submit" name="submit" value="Verstuur" class="btn btn-primary pull-right"><br><br><br>
                 </form>
             </div>
-            <div class="col-md-4 col-xs-12 well">
-                <h3>Contact informatie <strong>EKC 2000</strong></h3><hr>
-                <p class="lead">
-                    Straat: zwanenveld 5<br>
-                    Postcode: 7827 XA<br>
-                    Plaats: Emmen<br>
-                    Telefoonnummer: 0591-617979<br>
-                    Email: ekc2000.emmen@knkv.nl</p>
+            <div class="col-md-4 col-xs-12">
+                <div class="col-md-12 col-xs-12 well">
+                    <h3>Contact informatie <strong>EKC 2000</strong></h3><hr>
+                    <p class="lead">
+                        Straat: zwanenveld 5<br>
+                        Postcode: 7827 XA<br>
+                        Plaats: Emmen<br>
+                        Telefoonnummer: 0591-617979<br>
+                        Email: ekc2000.emmen@knkv.nl
+                    </p>
+                </div>
             </div>
         </div>
     </div>
