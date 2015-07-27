946dc8a1ac5fac45f20cbec55a1b36bb84179a34
martijn-mac <martijn13795@hotmail.com>
2015-07-27 18:20:06 +0200

    test

--- sub-pagina/403.php
+++ sub-pagina/403.php
@@ -1,4 +1,4 @@
-<?php include 'eck2000/includes/html.php';?>
+<?php include 'http://eck2000.party/includes/html.php';?>
 <div class="container">
     <div class="col-md-12 col-xs-12">
         <h1>Oeps! - U heeft niet het rechten om deze pagina te bezoeken</h1>
