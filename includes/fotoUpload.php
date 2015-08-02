4be6bfbc516ec30621a51bc3582b27a9e8f31598
martijn13795 <martijn13795@hotmail.com>
2015-08-02 22:03:32 +0200

    check

--- includes/fotoUpload.php
+++ includes/fotoUpload.php
@@ -1,60 +1,64 @@
 <?php
-if(!empty($_FILES['files']['name'][0])) {
+if($_POST['name'] == "" || $_POST['name'] == " " || $_POST['name'] == null){
 
-    $files = $_FILES['files'];
+}else {
+    if (!empty($_FILES['files']['name'][0])) {
 
-    $uploaded = array();
-    $failed = array();
+        $files = $_FILES['files'];
 
-    $allowed = array('jpg', 'jpeg', 'png');
+        $uploaded = array();
+        $failed = array();
 
-    foreach($files['name'] as $position => $file_name) {
+        $allowed = array('jpg', 'jpeg', 'png');
 
-        $file_tmp = $files['tmp_name'][$position];
-        $file_size = $files['size'][$position];
-        $file_error = $files['error'][$position];
+        foreach ($files['name'] as $position => $file_name) {
 
-        $file_ext = explode('.', $file_name);
-        $file_ext = strtolower(end($file_ext));
+            $file_tmp = $files['tmp_name'][$position];
+            $file_size = $files['size'][$position];
+            $file_error = $files['error'][$position];
 
-        if(in_array($file_ext, $allowed)) {
+            $file_ext = explode('.', $file_name);
+            $file_ext = strtolower(end($file_ext));
 
-            if($file_error === 0) {
+            if (in_array($file_ext, $allowed)) {
 
-                if($file_size <= 2097152) {
+                if ($file_error === 0) {
 
-                    $file_name_new = $file_name;
-                    $albumName = $_POST['name'];
-                    if (file_exists("../images/fotogalerij/" . $albumName . "/")){}
-                    else {
-                        mkdir("../images/fotogalerij/" . $albumName, 0777);
-                    }
-                    $file_destination = '../images/fotogalerij/' . $albumName . "/" . $file_name_new;
+                    if ($file_size <= 2097152) {
 
-                    if(move_uploaded_file($file_tmp, $file_destination)) {
-                        $uploaded[$position] = $file_destination;
+                        $file_name_new = $file_name;
+                        $albumName = $_POST['name'];
+                        if (file_exists("../images/fotogalerij/" . $albumName . "/")) {
+                        } else {
+                            mkdir("../images/fotogalerij/" . $albumName, 0777);
+                        }
+                        $file_destination = '../images/fotogalerij/' . $albumName . "/" . $file_name_new;
+
+                        if (move_uploaded_file($file_tmp, $file_destination)) {
+                            $uploaded[$position] = $file_destination;
+                        } else {
+                            $failed[$position] = $file_name . ", uploaden mislukt";
+                        }
+
                     } else {
-                        $failed[$position] = $file_name . ", uploaden mislukt";
+                        $failed[$position] = $file_name . "is te groot";
                     }
 
                 } else {
-                    $failed[$position] = $file_name . "is te groot";
+                    $failed[$position] = $file_name . "error" . $file_error;
                 }
 
             } else {
-                $failed[$position] = $file_name . "error" . $file_error;
+                $failed[$position] = $file_name . "kies een ander bestand type" . $file_ext;
             }
-
-        } else {
-            $failed[$position] = $file_name . "kies een ander bestand type" . $file_ext;
         }
-    }
 
-    if(!empty($uploaded)) {
-        print_r($uploaded);
-    }
+        if (!empty($uploaded)) {
+            print_r($uploaded);
+        }
 
-    if (!empty($failed)) {
-        print_r($failed);
+        if (!empty($failed)) {
+            print_r($failed);
+        }
     }
 }
