0d0309332ea17fc8981af0874950263367b696b6
martijn13795 <martijn13795@hotmail.com>
2015-08-05 14:48:21 +0200

    test

--- sub-pagina/album.php
+++ sub-pagina/album.php
@@ -40,7 +40,7 @@
                             <div class="change galerijBackImg" style="background-image: url('. $imgPath .')"
                                  data-src-320px="'. $imgPath .'"
                                  data-src-960px="'. $imgPath .'"
-                                 ><img src="'. $imgPath .'" alt="'. substr($imgPath, strrpos($imgPath, '/') + 1) .'" style="width: "col-md-3"/>
+                                 ><img src="'. $imgPath .'" alt="'. substr($imgPath, strrpos($imgPath, '/') + 1) .'" class="col-md-12"/>
                             </div>
                         </a>
                     </div>
