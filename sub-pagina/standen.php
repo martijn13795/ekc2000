4ffdb05c8d8b03fb656df5e877114db225b922ac
martijn13795 <martijn13795@hotmail.com>
2015-08-19 00:44:30 +0200

    antilopen shizzle

--- sub-pagina/standen.php
+++ sub-pagina/standen.php
@@ -1,4 +1,6 @@
 <?php include '../includes/html.php';?>
     <div class="container">
-
+        <div>
+            <object class="antilopen" type="text/html" data="http://www.antilopen.nl/competitie/standen.asp?ci=54&comp=1314zaal"></object>
+        </div>
     </div>
