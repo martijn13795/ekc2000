e252c5a24f8b32bdd82c9ead53821e8fcf0c5eab
martijn13795 <martijn13795@hotmail.com>
2015-08-18 16:27:08 +0200

    home page margin fix, menu typo fix, css cleanup, new contact page

--- sub-pagina/contact.php
+++ sub-pagina/contact.php
@@ -1,31 +1,46 @@
 <?php include '../includes/html.php';?>
     <div class="container">
-        <h1>Contact</h1><hr>
+        <h1>Contact informatie <strong>EKC 2000</strong></h1><hr>
         <div class="row">
-            <div class="col-md-8 col-xs-12">
-                <form action="" method="post">
-                    <label>Naam: </label><input type="text" class="form-control contactMargin" name="name" placeholder="Naam" REQUIRED><br>
-                    <label>Email: </label><input type="email" class="form-control contactMargin" name="email" placeholder="Email" REQUIRED><br>
-                    <label>Bericht: </label><textarea rows="6" class="form-control contactMargin" placeholder="Type hier uw bericht..." maxlength="2000" REQUIRED></textarea><br>
-                    <input type="submit" name="submit" value="Verstuur" class="btn btn-primary pull-right"><br><br><br>
-                </form>
-            </div>
-            <div class="col-md-4 col-xs-12">
+            <div class="col-md-7 col-xs-12">
                 <div class="col-md-12 col-xs-12 well">
-                    <h3>Contact informatie <strong>EKC 2000</strong></h3><hr>
                     <h3><strong>Veld</strong></h3>
                     <p class="lead">
+                        Kantine 'De uitwijk'<br>
+                        Plaats: Emmen<br>
                         Straat: zwanenveld 5<br>
-                        Postcode: 7827 XA Emmen<br>
+                        Postcode: 7827 XA<br>
                         Telefoonnummer: 0591-617979
-                    </p>
+                    </p><hr>
                     <h3><strong>Zaal</strong></h3>
                     <p class="lead">
+                        Plaats: Bargeres<br>
                         Straat: Calthornerbrink<br>
                         Postcode: 7812 HS Emmen<br>
                         Telefoonnummer: 0591-618868
                     </p>
                 </div>
             </div>
+            <div class="col-md-5 col-xs-12">
+                <div id="map"></div>
+            </div>
         </div>
     </div>
+<script>
+
+    var map;
+    function initMap() {
+        var myLatLng = {lat: 52.7470678, lng: 6.8850779};
+        map = new google.maps.Map(document.getElementById('map'), {
+            center: myLatLng,
+            zoom: 16
+        });
+        var marker = new google.maps.Marker({
+            position: myLatLng,
+            map: map,
+            title: 'EKC 2000'
+        });
+    }
+
+</script>
+<script src="https://maps.googleapis.com/maps/api/js?callback=initMap" async defer></script>
