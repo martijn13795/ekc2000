8bed8704c40a79b7858c6b55155821409457b498
martijn13795 <martijn13795@hotmail.com>
2015-08-21 15:25:25 +0200

    ckeditor nieuws page

--- sub-pagina/nieuws.php
+++ sub-pagina/nieuws.php
@@ -1,4 +1,16 @@
+<script src="../ckeditor/ckeditor.js"></script>
 <?php include '../includes/html.php';?>
-    <div class="container">
+    <div class="container"><br>
+        <form action="" method="POST">
+            <textarea class="ckeditor" name="editor1"></textarea><br>
+            <input type="submit" value="Upload"/>
+        </form>
+        <?php
+        if($_POST) {
+            $text = $_POST['editor1'];
 
+            echo $text;
+        }
+
+        ?>
     </div>
