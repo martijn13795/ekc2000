ad941e09b1bb1550cb0185c3a6a5aa832fdc0b08
martijn13795 <martijn13795@hotmail.com>
2015-08-22 21:39:55 +0200

    antilopen

--- sub-pagina/kantinedienst.php
+++ sub-pagina/kantinedienst.php
@@ -1,4 +1,6 @@
 <?php include '../includes/html.php';?>
     <div class="container">
-
+        <div>
+            <object class="antilopen" type="text/html" data="http://www.antilopen.nl/competitie/diensten.asp?ci=54&d=355&dagen=14"></object>
+        </div>
     </div>
