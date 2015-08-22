e2dadcea17582bf4d634f1b05ccba196fb695c52
cassshh <casvd@hotmail.com>
2015-08-22 17:38:08 +0200

    Mhm

--- includes/removeMessage.php
+++ includes/removeMessage.php
@@ -7,27 +7,6 @@ if($user->isLoggedIn() && $user->hasPermission('admin')){
         $id = $_GET['id'];
         if($db->query("SELECT * FROM messages WHERE id = '$id'")->count()){
             $db->query("DELETE FROM messages WHERE id = '$id'");
-            ?>
-            <script>
-                $('.alert').remove();
-                var tmpl = '<div class="alert alert-success alert-dismissable">'+
-                    '<button class="close" data-dismiss="alert">&times;</button>'+
-                    'Bericht is succesvol verwijderd.'+
-                    '</div>';
-                $('.row-fluid').append(tmpl);
-                setTimeout(function(){
-                    $('.alert').addClass('on');
-                    setTimeout(function(){
-                        $('.alert').removeClass('on');
-                        setTimeout(function(){
-                            $('.alert').remove();
-                        },1000);
-                    },5000);
-                },10);
-
-                $('#chatRefresh').load('/includes/chatRefresh.php');
-            </script>
-            <?php
         }
     }
 }
