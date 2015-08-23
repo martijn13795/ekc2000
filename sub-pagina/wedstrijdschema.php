60f35aabe0c743a9eaef66a645aa02241819bd40
martijn13795 <martijn13795@hotmail.com>
2015-08-23 14:50:12 +0200

    iframe vs object

--- sub-pagina/wedstrijdschema.php
+++ sub-pagina/wedstrijdschema.php
@@ -1,6 +1,7 @@
 <?php include '../includes/html.php';?>
     <div class="container">
         <div>
+            <iframe border="0" width="100%" height="500px" frameborder="0" src="http://www.antilopen.nl/competitie/programma.asp?ci=54&clubstyle=EKC2000&kolomkoppen=true"></iframe>
             <object class="antilopen" type="text/html" data="http://www.antilopen.nl/competitie/programma.asp?ci=54&clubstyle=EKC2000&kolomkoppen=true"></object>
         </div>
     </div>
