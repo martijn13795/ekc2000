1a38a635d1f6eee9a868ea359a5909f1c9ad4dc1
martijn13795 <martijn13795@hotmail.com>
2015-09-07 23:07:05 +0200

    url fix

--- includes/nieuwsUpload.php
+++ includes/nieuwsUpload.php
@@ -2,6 +2,7 @@
 require_once $_SERVER['DOCUMENT_ROOT'] . '/core/init.php';
 $db = DB::getInstance();
 $name = $_POST['artikelName'];
+$name = str_replace(' ', '-', $name);
 $text = $_POST['editor1'];
 $date = date("Y-m-d H:i:s");
 $db->query("INSERT INTO nieuws (name, text, date) VALUES ('$name', '$text', '$date')");
