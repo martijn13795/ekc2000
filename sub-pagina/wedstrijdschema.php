0dbc668282890d82e63e9e1209e13b5a3bcdbeff
martijn13795 <martijn13795@hotmail.com>
2015-10-07 14:56:37 +0200

    clean wedstrijdschema

--- sub-pagina/wedstrijdschema.php
+++ sub-pagina/wedstrijdschema.php
@@ -4,22 +4,3 @@
             <iframe class="antilopen" border="0"frameborder="0" src="http://www.antilopen.nl/competitie/programma.asp?ci=54&clubstyle=EKC2000&kolomkoppen=true"></iframe>
         </div>
     </div>
-
-<script language="JavaScript">
-    <!--
-    function autoResize(id){
-        var newheight;
-        var newwidth;
-
-        if(document.getElementById){
-            newheight=document.getElementById(id).contentWindow.document .body.scrollHeight;
-            newwidth=document.getElementById(id).contentWindow.document .body.scrollWidth;
-        }
-
-        document.getElementById(id).height= (newheight) + "px";
-        document.getElementById(id).width= (newwidth) + "px";
-    }
-    //-->
-</script>
-
-<IFRAME SRC="http://www.antilopen.nl/competitie/programma.asp?ci=54&clubstyle=EKC2000&kolomkoppen=true" width="100%" height="200px" id="iframe1" marginheight="0" frameborder="0" onLoad="autoResize('iframe1');"></iframe>
