05d40ae6d6d6d4232b4333fc99264b9b45eece47
martijn13795 <martijn13795@hotmail.com>
2015-10-16 13:55:47 +0200

    trainingstijden en wedstrijdverslagen

--- sub-pagina/trainingstijden.php
+++ sub-pagina/trainingstijden.php
@@ -1,4 +1,98 @@
 <?php include '../includes/html.php';?>
     <div class="container">
-
+        <h1>Trainingstijden</h1><hr>
+        <div class="col-md-12 col-xs-12">
+            <table class="table table-bordered table-hover">
+                <thead>
+                <tr>
+                    <th scope="row" class="tableHeader">Teams</th>
+                    <th scope="row" class="tableHeader" colspan="5">Trainingstijden</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <th scope="row" class="tableHeader"></th>
+                    <th scope="row" class="tableHeader">Maandag</th>
+                    <th scope="row" class="tableHeader">Dinsdag</th>
+                    <th scope="row" class="tableHeader">Woensdag</th>
+                    <th scope="row" class="tableHeader">Donderdag</th>
+                    <th scope="row" class="tableHeader">Vrijdag</th>
+                </tr>
+                <tr>
+                    <th scope="row">1<sup>e</sup></th>
+                    <td></td>
+                    <td>20:00 t/m 21:30</td>
+                    <td></td>
+                    <td></td>
+                    <td>20:00 t/m 21:30</td>
+                </tr>
+                <tr>
+                    <th scope="row">2<sup>e</sup></th>
+                    <td></td>
+                    <td>20:00 t/m 21:30</td>
+                    <td></td>
+                    <td></td>
+                    <td>20:00 t/m 21:30</td>
+                </tr>
+                <tr>
+                    <th scope="row">3<sup>e</sup></th>
+                    <td></td>
+                    <td>20:00 t/m 21:30</td>
+                    <td></td>
+                    <td></td>
+                    <td>20:00 t/m 21:30</td>
+                </tr>
+                <tr>
+                    <th scope="row">A's</th>
+                    <td></td>
+                    <td>20:00 t/m 21:30</td>
+                    <td></td>
+                    <td></td>
+                    <td>20:00 t/m 21:30</td>
+                </tr><tr>
+                    <th scope="row">B's</th>
+                    <td></td>
+                    <td>20:00 t/m 21:30</td>
+                    <td></td>
+                    <td></td>
+                    <td>20:00 t/m 21:30</td>
+                </tr><tr>
+                    <th scope="row">C's</th>
+                    <td></td>
+                    <td>20:00 t/m 21:30</td>
+                    <td></td>
+                    <td></td>
+                    <td>20:00 t/m 21:30</td>
+                </tr><tr>
+                    <th scope="row">D's</th>
+                    <td></td>
+                    <td>20:00 t/m 21:30</td>
+                    <td></td>
+                    <td></td>
+                    <td>20:00 t/m 21:30</td>
+                </tr><tr>
+                    <th scope="row">E's</th>
+                    <td></td>
+                    <td>20:00 t/m 21:30</td>
+                    <td></td>
+                    <td></td>
+                    <td>20:00 t/m 21:30</td>
+                </tr><tr>
+                    <th scope="row">F's</th>
+                    <td></td>
+                    <td>20:00 t/m 21:30</td>
+                    <td></td>
+                    <td></td>
+                    <td>20:00 t/m 21:30</td>
+                </tr><tr>
+                    <th scope="row">Welpen</th>
+                    <td></td>
+                    <td>20:00 t/m 21:30</td>
+                    <td></td>
+                    <td></td>
+                    <td>20:00 t/m 21:30</td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
     </div>
--- sub-pagina/wedstrijdverslagen.php
+++ sub-pagina/wedstrijdverslagen.php
@@ -1,4 +1,12 @@
 <?php include '../includes/html.php';?>
     <div class="container">
-
+        <h1>Wedstrijdverslagen</h1><hr>
+        <div class="col-md-12 col-xs-12">
+            <form>
+                <label>Naam van het verslag:</label>
+                <input type="text" class="form-control" placeholder="Naam" REQUIRED/><br>
+                <textarea class="form-control" rows="6" placeholder="Type hier..." REQUIRED></textarea></br>
+                <input type="submit" class="btn btn-success"/>
+            </form>
+        </div>
     </div>
