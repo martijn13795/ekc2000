2375c99f577cdba016d25183d36a1c35ddaa27ed
martijn13795 <martijn13795@hotmail.com>
2015-10-20 16:29:26 +0200

    documenten upoaden :trollface:

---
+++ includes/documentUpload.php
@@ -0,0 +1,49 @@
+<?php
+require_once $_SERVER['DOCUMENT_ROOT'] . '/core/init.php';
+$db = DB::getInstance();
+
+$target_dir = "../documents/";
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+// Check if image file is a actual image or fake image
+if(isset($_POST["submit"])) {
+    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+    if($check !== false) {
+        $uploadOk = 1;
+    } else {
+        $uploadOk = 0;
+    }
+}
+// Check if file already exists
+if (file_exists($target_file)) {
+    echo "Het bestand bestaat al";
+    $uploadOk = 0;
+}
+// Check file size
+if ($_FILES["fileToUpload"]["size"] > 5000000) {
+    echo "Het bestand is te groot";
+    $uploadOk = 0;
+}
+// Allow certain file formats
+if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+    && $imageFileType != "doc" && $imageFileType != "docx" && $imageFileType != "xls" && $imageFileType != "xlsx" && $imageFileType != "ppt" && $imageFileType != "pptx" && $imageFileType != "pdf") {
+    echo "Alleen JPG, JPEG, PNG, DOC, DOCX, XLS, XLSX, PPT, PPTX & PDF files mogen worden geupload";
+    $uploadOk = 0;
+}
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+    echo "<br><br>Het bestand is niet geupload";
+// if everything is ok, try to upload file
+} else {
+    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+        $db->insert('documents',array(
+            'name' => $_POST['name'],
+            'date' => date("Y-m-d H:i:s"),
+            'path' => $target_file
+        ));
+        echo basename( $_FILES["fileToUpload"]["name"]). " is geupload";
+    } else {
+        echo "Er is een error opgetreden";
+    }
+}
--- sub-pagina/documenten.php
+++ sub-pagina/documenten.php
@@ -1,10 +1,55 @@
 <?php include '../includes/html.php'; if ($user->isLoggedIn()) { ?>
+<script src="http://malsup.github.com/jquery.form.js"></script>
     <div class="container">
         <h1>Documenten</h1><hr>
-        <form method="post" action="">
-            <label>Naam van Document:</label><input type="text" id="name" class="form-control" name="name" placeholder="Naam" maxlength="60" REQUIRED><br>
-            <input type="file" id="file" name="files" REQUIRED><br>
-            <input type="submit" class="btn btn-success">
-        </form>
+        <div class="col-md-12 col-xs-12">
+            <div class="hidden visible-lg">
+                <form method="post" action="../includes/documentUpload.php" name="myForm" class="myForm" enctype="multipart/form-data">
+                    <label>Naam van Document:</label><input type="text" id="name" class="form-control" name="name" placeholder="Naam" maxlength="60" REQUIRED><br>
+                    <input type="file" id="fileToUpload" name="fileToUpload" REQUIRED><br>
+                    <input type="submit" id="submit" class="btn btn-success">
+                </form>
+                <button class="btn btn-info" id="refresh" onclick="history.go(0)">Refresh</button><br><br>
+                <div id="error"></div>
+            </div>
+            <?php
+            }
+            $db = DB::getInstance();
+            $documents = $db->query("SELECT * FROM documents ORDER BY date DESC");
+            if ($documents->count()) {
+                foreach ($documents->results() as $document) {
+                    $image = $document->path;
+                    $type = substr($document->path, -3);
+                    if($type == "doc" OR $type == "ocx"){$image = "../images/doc.png";}
+                    if($type == "xls" OR $type == "lsx"){$image = "../images/xls.png";}
+                    if($type == "ppt" OR $type == "ptx"){$image = "../images/ppt.png";}
+                    if($type == "pdf"){$image = "../images/pdf.png";}
+                        echo '<div class="well albumsDiv"><a href="/' . $document->path . '"><img class="roundImg" src="' . $image . '"/><h3>'
+                            . escape(str_replace('-', ' ', $document->name)) . '</h3></a><p>Geupload op: ' . escape(explode(" ", $document->date)[0]) . '</p><br></div>';
+                }
+            } else {
+                echo '<div class="well albumsDiv"><br/><h3>Er zijn nog geen documenten beschikbaar</h3></div>';
+            }
+            ?>
+        </div>
     </div>
-<?php } ?>
+<script>
+    $(document).ready(function () {
+        $('.myForm').ajaxForm({
+            beforeSend: function () {
+                $("#submit").hide();
+                $("#error").show();
+                $("#error").html('<h3>Even geduld alstublieft</h3><p>Refresh de pagina niet</p>');
+            },
+            success: function (response) {
+                $("#refresh").show();
+                $("#error").show();
+                $("#error").html(response);
+                $("#name").val('');
+                $("#fileToUpload").val('');
+            }
+        });
+        $("#refresh").hide();
+        $("#error").hide();
+    });
+</script>
