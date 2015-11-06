517982cbc08dff3ed2c2d7ea7890c3ef4b78196c
martijn13795 <martijn13795@hotmail.com>
2015-11-06 16:51:25 +0100

    wedstrijdversleagen uploaden

--- includes/wedstrijdverslagenUpload.php
+++ includes/wedstrijdverslagenUpload.php
@@ -1,3 +1,24 @@
 <?php
 require_once $_SERVER['DOCUMENT_ROOT'] . '/core/init.php';
 $db = DB::getInstance();
+$user = new User();
+
+if($user->isLoggedIn()) {
+    if (!empty($_POST['name']) && isset($_POST['name'])
+        && !empty($_POST['text']) && isset($_POST['text'])
+        && !empty($_POST['team']) && is_numeric($_POST['team'])
+    ) {
+        $name = trim($_POST['name']);
+        $text = trim($_POST['text']);
+        $team_id = trim($_POST['team']);
+        $db->insert('reports',array(
+            'user_id' => $user->data()->id,
+            'name' => $name,
+            'text' => $text,
+            'team_id' => $team_id,
+            'date' => date("Y-m-d H:i:s")));
+        echo "<p>Het verslag is verstuurd</p><br>";
+    } else {
+        echo "<h3>Er is wat mis gegaan. Probeer het opnieuw.</h3><br>";
+    }
+}
--- sub-pagina/wedstrijdverslagen.php
+++ sub-pagina/wedstrijdverslagen.php
@@ -2,10 +2,11 @@
 <script src="http://malsup.github.com/jquery.form.js"></script>
     <div class="container">
         <h1>Wedstrijdverslagen</h1><hr>
+        <?php if ($user->isLoggedIn()) { ?>
         <div class="col-md-12 col-xs-12">
             <form method="post" name="myForm" class="myForm" action="../includes/wedstrijdverslagenUpload.php">
                 <label>Naam van het verslag:</label>
-                <input type="text" class="form-control" placeholder="Naam" id="name" REQUIRED/><br>
+                <input type="text" class="form-control" placeholder="Naam" id="name" name="name" REQUIRED/><br>
                 <label>Van welk team is het verslag:</label>
                 <select name="team" class="form-control" id="team" REQUIRED>
                     <option disabled selected value="">Selecteer een team</option>
@@ -25,11 +26,29 @@
                     }
                     ?>
                 </select><br>
-                <textarea class="form-control" rows="8" id="verslag" placeholder="Type hier..." REQUIRED></textarea></br>
+                <textarea class="form-control" rows="8" id="text" name="text" placeholder="Type hier..." REQUIRED></textarea></br>
                 <input type="submit" id="submit" class="btn btn-success">
             </form>
             <button class="btn btn-info" id="refresh" onclick="history.go(0)">Refresh</button><br><br>
             <div id="error"></div>
+        </div>
+        <?php
+        }
+        ?>
+        <div class="col-md-12 col-xs-12">
+            <?php
+            if ($user->isLoggedIn() && $user->hasPermission('dev')) {
+                $db = DB::getInstance();
+                $reports = $db->query("SELECT * FROM reports ORDER BY date DESC");
+                if ($reports->count()) {
+                    foreach ($reports->results() as $report) {
+                        echo '<div class="well activiteitDiv"><h3>' . escape(str_replace('-', ' ', $report->name)) . '</h3><p>Upload datum: ' . escape(explode(" ", $report->date)[0]) . '</p></div>';
+                    }
+                } else {
+                    echo '<div class="well activiteitDiv"><br><h3>Er zijn nog geen verslagen beschikbaar</h3></div>';
+                }
+            }
+            ?>
         </div>
     </div>
 <script>
@@ -46,7 +65,7 @@
                 $("#error").html(response);
                 $("#name").val('');
                 $("#team").val('');
-                $("#verslag").val('');
+                $("#text").val('');
             }
         });
         $("#refresh").hide();
