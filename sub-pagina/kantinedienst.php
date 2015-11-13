43cf79f3e52981aed27a46dc10892c3cfa9aa60c
martijn13795 <martijn13795@hotmail.com>
2015-11-13 18:07:04 +0100

    new layout zaal-/kantinedienst

--- sub-pagina/kantinedienst.php
+++ sub-pagina/kantinedienst.php
@@ -1,7 +1,28 @@
 <?php include '../includes/html.php';?>
     <div class="container">
+        <h1>Zaal-/kantinedienst</h1>
         <div>
-            <iframe class="antilopen" border="0"frameborder="0" src="http://www.antilopen.nl/competitie/diensten.asp?ci=54&d=355&inschrijven=false&alles=true&clubstyle=EKC2000"></iframe>
+            <?php
+            $url = "http://www.antilopen.nl/competitie/diensten.asp?ci=54&d=355&inschrijven=false&alles=true&clubstyle=EKC2000";
+
+            $str = file_get_contents($url);
+
+            function get_url_contents($url){
+                $crl = curl_init();
+                $timeout = 5;
+                curl_setopt ($crl, CURLOPT_URL,$url);
+                curl_setopt ($crl, CURLOPT_RETURNTRANSFER, 1);
+                curl_setopt ($crl, CURLOPT_CONNECTTIMEOUT, $timeout);
+                $ret = curl_exec($crl);
+                curl_close($crl);
+                return $ret;
+            }
+
+            echo get_url_contents($url);
+            ?>
         </div>
     </div>
 <?php include '../includes/htmlUnder.php'; ?>
+<script>
+    $('table').addClass("table table-striped");
+</script>
