8905749fc69c2d0228916ba1ce49e36b4b976241
martijn13795 <martijn13795@hotmail.com>
2015-11-16 22:01:28 +0100

    just for lolz

--- sub-pagina/info.php
+++ sub-pagina/info.php
@@ -1,46 +1,47 @@
 <?php
 include '../includes/html.php';
+
 if ($user->isLoggedIn()) {
     if ($user->hasPermission('dev')) {
         ?>
         <div class="container">
             <h1>Info</h1><hr>
-            <table class="table table-bordered table-hover table_sort">
-            <thead>
-            <tr>
-                <th class="tableHeader">ID</th>
-                <th class="tableHeader">IP</th>
-                <th class="tableHeader">Name</th>
-                <th class="tableHeader">City</th>
-                <th class="tableHeader">Region</th>
-                <th class="tableHeader">Country</th>
-                <th class="tableHeader">Date</th>
-            </tr>
-            </thead>
-            <tbody>
-            <?php
-            $visitors = $db->query("SELECT * FROM visitors WHERE country = 'Netherlands' ORDER BY id DESC");
-            if ($visitors->count()) {
-                $totalNL = 0;
-                foreach ($visitors->results() as $visitor) {
-                    $totalNL++;
-                    echo '
-                        <tr>
-                            <td>' . escape($visitor->id) . '</td>
-                            <td>' . escape($visitor->ip) . '</td>
-                            <td>' . escape($visitor->name) . '</td>
-                            <td>' . escape($visitor->city) . '</td>
-                            <td>' . escape($visitor->region) . '</td>
-                            <td>' . escape($visitor->country) . '</td>
-                            <td>' . escape($visitor->date) . '</td>
-                        </tr>
-                         ';
+            <table class="table table-striped table-bordered myTable">
+                <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>IP</th>
+                    <th>Name</th>
+                    <th>City</th>
+                    <th>Region</th>
+                    <th>Country</th>
+                    <th>Date</th>
+                </tr>
+                </thead>
+                <tbody>
+                <?php
+                $visitors = $db->query("SELECT * FROM visitors WHERE country = 'Netherlands' ORDER BY id DESC");
+                if ($visitors->count()) {
+                    $totalNL = 0;
+                    foreach ($visitors->results() as $visitor) {
+                        $totalNL++;
+                        echo '
+                            <tr>
+                                <td>' . escape($visitor->id) . '</td>
+                                <td>' . escape($visitor->ip) . '</td>
+                                <td>' . escape($visitor->name) . '</td>
+                                <td>' . escape($visitor->city) . '</td>
+                                <td>' . escape($visitor->region) . '</td>
+                                <td>' . escape($visitor->country) . '</td>
+                                <td>' . escape($visitor->date) . '</td>
+                            </tr>
+                             ';
+                    }
+                    echo "Aantal resultaten: " . $totalNL . "<br><br>";
                 }
-                echo "Aantal resultaten: " . $totalNL . "<br><br>";
-            }
-            ?>
-            </tbody>
-            </table>
+                ?>
+                </tbody>
+            </table><br><br><br><br>
         </div>
         <?php
     } else {
@@ -70,12 +71,17 @@ if ($user->isLoggedIn()) {
 <?php
 }
 ?>
-<script src="../js/jquery.table-sort.js"></script>
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/s/dt/dt-1.10.10/datatables.min.css"/>
+    <link rel="stylesheet" type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css"/>
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.10/css/dataTables.bootstrap.min.css"/>
+    <script type="text/javascript" src="//code.jquery.com/jquery-1.11.3.min.js"></script>
+    <script type="text/javascript" src="https://cdn.datatables.net/1.10.10/js/jquery.dataTables.min.js"></script>
+    <script type="text/javascript" src="https://cdn.datatables.net/1.10.10/js/dataTables.bootstrap.min.js"></script>
 <script>
-    $(function(){
-        $("table.table_sort").sort_table({
-            "action" : "init"
-        });
-    });
+    $(document).ready(function() {
+        $('.myTable').DataTable();
+    } );
+
+    $('th').css('background', 'transparent');
 </script>
 <?php include '../includes/htmlUnder.php'; ?>
