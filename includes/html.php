cf7e84f5d338b056819d04bbdbc69974b21bf013
martijn13795 <martijn13795@hotmail.com>
2016-03-20 21:36:37 +0100

    Jquery update
    Fixes huge bug with hamburger menu mobile

--- includes/html.php
+++ includes/html.php
@@ -49,9 +49,8 @@ Cas van Dinter
     <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
-    <script src="../js/jquery-2.1.4.min.js" type="text/javascript"></script>
+    <script src="../js/jquery-2.2.2.min.js" type="text/javascript"></script>
     <script src="../js/my_script.js" type="text/javascript"></script>
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.js"></script>
     <script src="../js/bootstrap.min.js"></script>
 </head>
 <body>
