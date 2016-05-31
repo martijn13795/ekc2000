479d4f61c8a48073a2c9ad860758cd9d918ade9f
martijn13795 <martijn13795@hotmail.com>
2016-05-31 21:55:33 +0200

    DB fix

--- core/init.php
+++ core/init.php
@@ -4,7 +4,7 @@ if(!isset($_SESSION)){session_start();}
 
 $GLOBALS['config'] = array(
     'mysql' => array(
-        'host' => '94.213.96.73',
+        'host' => 'localhost',
         'username' => 'ekc2000',
         'password' => 'xH2b8C5PnajhnXJ5',
         'db' => 'ekc'
