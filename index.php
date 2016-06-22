2253700411c52c14f7eea663d50073a498126409
martijn13795 <martijn13795@hotmail.com>
2016-06-22 23:32:09 +0200

    Sponsor kliks

--- index.php
+++ index.php
@@ -214,6 +214,7 @@
                     </div>
                 </div>
                 <div class="col-md-4 col-xs-12 chatRow">
+                    <a href="http://www.sponsorkliks.com/winkels.php?club=3554" target="_blank"><img src="http://www.sponsorkliks.com/gfx/sk_lr_logos/licht_200_200.gif" class="img-responsive" style="margin: 0 auto;" alt="SponsorKliks, gratis sponsoren!" title="Sponsor EKC 2000 gratis!" Border="0"></a>
                     <h4>Bekijk de tweets van EKC 2000 live!</h4>
                     <a class="twitter-timeline" href="https://twitter.com/EKC2000_Emmen" data-widget-id="618144740459065344">Tweets door @EKC2000_Emmen</a>
                     <script type="text/javascript">!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs")</script>
