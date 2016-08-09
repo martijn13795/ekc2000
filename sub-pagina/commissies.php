99669fa0f6eb0d76dd32610d28092f3febd7b149
martijn13795 <martijn13795@hotmail.com>
2016-08-09 02:55:44 +0200

    commissies.php works
    Design moet nog

--- sub-pagina/commissies.php
+++ sub-pagina/commissies.php
@@ -2,92 +2,60 @@
     <div class="container">
         <div class="col-md-12 col-xs-12">
             <h1>De commissies van EKC 2000</h1><hr>
-            <div class="col-md-5 col-xs-12 well commissieDiv">
-                <h3>Technische commissie:</h3>
-                <p>Petra Bethlehem<br>
-                    Ilse Schuttrups<br>
-                    Audrey Root-Muskee<br>
-                    Alex Veenstra<br>
-                    Riejander Deen<br>
-                    Gert Pruim</p>
-            </div>
-            <div class="col-md-5 col-md-offset-2 col-xs-12 well commissieDiv">
-                <h3>PR- & Communicatie-commissie:</h3>
-                <p>Jan Westerhof<br>
-                    Redmer Bouma<br>
-                    David Sleeking<br>
-                    Bert Schepers</p>
-            </div>
-            <div class="col-md-5 col-xs-12 well commissieDiv">
-                <h3>Sponsorcommissie:</h3>
-                <p>Erik Post<br>
-                    Jelmer Bouma<br>
-                    Roel Hilbrands</p>
-            </div>
-            <div class="col-md-5 col-md-offset-2 col-xs-12 well commissieDiv">
-                <h3>Jeugdactiviteitencommissie:</h3>
-                <p>Mirjan Bischop<br>
-                    Gerda Pol<br>
-                    Maaike Smits<br>
-                    Patrick Posthuma<br>
-                    Margot Dol</p>
-            </div>
-            <div class="col-md-5 col-xs-12 well commissieDiv">
-                <h3>Kantinecommissie:</h3>
-                <p>Alex Veenstra<br>
-                    Theo Venema<br>
-                    Ina Venema<br>
-                </p>
-            </div>
-            <div class="col-md-5 col-md-offset-2 col-xs-12 well commissieDiv">
-                <h3>Ledenadministratie:</h3>
-                <p>Jolanda ten Broeke</p>
-            </div>
-            <div class="col-md-5 col-xs-12 well commissieDiv">
-                <h3>Kledingcommissie:</h3>
-                <p>Lizeth Timmerman<br>
-                    Bianca Heeres</p>
-            </div>
-            <div class="col-md-5 col-md-offset-2 col-xs-12 well commissieDiv">
-                <h3>Materiaalbeheer:</h3>
-                <p>Theo Venema<br>
-                    Harry Deen</p>
-            </div>
-            <div class="col-md-5 col-xs-12 well commissieDiv">
-                <h3>Schoolkorfbalcommissie:</h3>
-                <p>Petra Bethlehem<br>
-                    Harma Frans<br>
-                    Monique Hekhuis<br>
-                    Marloes Heijblom<br>
-                    Elona Dokter</p>
-            </div>
-            <div class="col-md-5 col-md-offset-2 col-xs-12 well commissieDiv">
-                <h3>Supportersvereniging:</h3>
-                <p>Peter Bult<br>
-                    Redmer Bouma<br>
-                    Bert Schepers<br>
-                    Alnie Deen<br>
-                    Sanne van Os</p>
-            </div>
-            <div class="col-md-5 col-xs-12 well commissieDiv">
-                <h3>Scheidsrechteraanwijzer:</h3>
-                <p>Michael Beekman</p>
-            </div>
-            <div class="col-md-5 col-md-offset-2 col-xs-12 well commissieDiv">
-                <h3>Wedstrijdsecretariaat Senioren/Jeugd:</h3>
-                <p>Petra Bethlehem<br>
-                    Ilse Schutrups</p>
-            </div>
-            <div class="col-md-5 col-xs-12 well commissieDiv">
-                <h3>Ledencommissie en vrijwilligers:</h3>
-                <p>Eva van Dongeren<br>
-                    Roel Hilbrands<br>
-                    Ralph van Straten</p>
-            </div>
-            <div class="col-md-5 col-md-offset-2 col-xs-12 well commissieDiv">
-                <h3>Halcommissie:</h3>
-                <p>Vacant</p>
-            </div>
+            <?php
+            $commissions = $db->query("SELECT * FROM commissions");
+            if ($commissions->count()) {
+                $i = 0;
+                foreach ($commissions->results() as $commission) {
+                    if ($i % 2 == 0) {
+                        echo '
+                        <div class="col-md-5 col-xs-12 well commissieDiv">
+                            <h3>' . escape($commission->name) . ':</h3>
+                            <p>Email: ' . escape($commission->mail) . '</p>
+                            '; if ($commission->phone != 0){ echo '<p>Telefoonnummer: ' . escape($commission->phone) . '</p>';}echo '
+                            $tags = explode(',,',$commission->members);
+                            foreach($tags as $key) {
+                                $tags2 = explode(',',$key);
+                                foreach($tags2 as $key2) {
+                                    if ($key2 != "") {
+                                        $getMembers = $db->query("SELECT * FROM users WHERE id='$key2'")->first();
+                                        echo $getMembers->name.' ';
+                                        echo $getMembers->surname_prefix.' ';
+                                        echo $getMembers->surname.'<br>';
+                                    }
+                                }
+                            }
+                        echo '</p>
+                        </div>
+                    ';
+                    } else {
+                        echo '
+                        <div class="col-md-5 col-md-offset-2 col-xs-12 well commissieDiv">
+                            <h3>' . escape($commission->name) . ':</h3>
+                            <p>Email: ' . escape($commission->mail) . '</p>
+                            '; if ($commission->phone != 0){ echo '<p>Telefoonnummer: ' . escape($commission->phone) . '</p>';}echo '
+                            $tags = explode(',,',$commission->members);
+                            foreach($tags as $key) {
+                                $tags2 = explode(',',$key);
+                                foreach($tags2 as $key2) {
+                                    if ($key2 != "") {
+                                        $getMembers = $db->query("SELECT * FROM users WHERE id='$key2'")->first();
+                                        echo $getMembers->name.' ';
+                                        echo $getMembers->surname_prefix.' ';
+                                        echo $getMembers->surname.'<br>';
+                                    }
+                                }
+                            }
+                        echo '</p>
+                        </div>
+                    ';
+                    }
+                    $i++;
+                }
+            } else {
+                echo "<h3>Er zijn nog geen commissies</h3>";
+            }
+            ?>
         </div>
     </div>
 <?php include '../includes/htmlUnder.php'; ?>
