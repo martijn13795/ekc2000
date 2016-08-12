77daa4b97b640b824a9be153ac7a46ca2e3387ee
martijn13795 <martijn13795@hotmail.com>
2016-08-12 02:46:00 +0200

    Commissies page new dedign

--- sub-pagina/commissies.php
+++ sub-pagina/commissies.php
@@ -9,46 +9,50 @@
                 foreach ($commissions->results() as $commission) {
                     if ($i % 2 == 0) {
                         echo '
-                        <div class="col-md-5 col-xs-12 well commissieDiv">
-                            <h3>' . escape($commission->name) . ':</h3>
-                            <p>Email: ' . escape($commission->mail) . '</p>
-                            '; if ($commission->phone != 0){ echo '<p>Telefoonnummer: ' . escape($commission->phone) . '</p>';}echo '
-                            <p>Leden:<br>';
-                            $tags = explode(',,',$commission->members);
-                            foreach($tags as $key) {
-                                $tags2 = explode(',',$key);
-                                foreach($tags2 as $key2) {
-                                    if ($key2 != "") {
-                                        $getMembers = $db->query("SELECT * FROM users WHERE id='$key2'")->first();
-                                        echo $getMembers->name.' ';
-                                        echo $getMembers->surname_prefix.' ';
-                                        echo $getMembers->surname.'<br>';
+                        <div class="row commissieRow">
+                            <div class="col-md-5 col-xs-12 well commissieDiv">
+                                <h3>' . escape($commission->name) . '</h3>
+                                <p><b>Email: </b><a href="mailto:' . escape($commission->mail) . '" style="color: #37c0fb;">' . escape($commission->mail) . '</a></p>
+                                '; if ($commission->phone != 0){ echo '<p><b>Telefoonnummer: </b>&plus;31 ' . escape($commission->phone) . '</p>';}echo '
+                                <p style="margin-bottom: -10px;"><b>Leden: </b><p>';
+                                $tags = explode(',,',$commission->members);
+                                foreach($tags as $key) {
+                                    $tags2 = explode(',',$key);
+                                    foreach($tags2 as $key2) {
+                                        if ($key2 != "") {
+                                            $getMembers = $db->query("SELECT * FROM users WHERE id='$key2'")->first();
+                                            echo "<p style='margin-bottom: -15px;'>&bull; ";
+                                            echo $getMembers->name.' ';
+                                            echo $getMembers->surname_prefix.' ';
+                                            echo $getMembers->surname.'</p><br>';
+                                        }
                                     }
                                 }
-                            }
-                        echo '</p>
-                        </div>
+                            echo '
+                            </div>
                     ';
                     } else {
                         echo '
-                        <div class="col-md-5 col-md-offset-2 col-xs-12 well commissieDiv">
-                            <h3>' . escape($commission->name) . ':</h3>
-                            <p>Email: ' . escape($commission->mail) . '</p>
-                            '; if ($commission->phone != 0){ echo '<p>Telefoonnummer: ' . escape($commission->phone) . '</p>';}echo '
-                            <p>Leden:<br>';
-                            $tags = explode(',,',$commission->members);
-                            foreach($tags as $key) {
-                                $tags2 = explode(',',$key);
-                                foreach($tags2 as $key2) {
-                                    if ($key2 != "") {
-                                        $getMembers = $db->query("SELECT * FROM users WHERE id='$key2'")->first();
-                                        echo $getMembers->name.' ';
-                                        echo $getMembers->surname_prefix.' ';
-                                        echo $getMembers->surname.'<br>';
+                            <div class="col-md-5 col-md-offset-2 col-xs-12 well commissieDiv">
+                                <h3>' . escape($commission->name) . '</h3>
+                                <p><b>Email: </b><a href="mailto:' . escape($commission->mail) . '" style="color: #37c0fb;">' . escape($commission->mail) . '</a></p>
+                                '; if ($commission->phone != 0){ echo '<p><b>Telefoonnummer: </b>&plus;31 ' . escape($commission->phone) . '</p>';}echo '
+                                <p style="margin-bottom: -10px;"><b>Leden: </b><p>';
+                                $tags = explode(',,',$commission->members);
+                                foreach($tags as $key) {
+                                    $tags2 = explode(',',$key);
+                                    foreach($tags2 as $key2) {
+                                        if ($key2 != "") {
+                                            $getMembers = $db->query("SELECT * FROM users WHERE id='$key2'")->first();
+                                            echo "<p style='margin-bottom: -15px;'>&bull; ";
+                                            echo $getMembers->name.' ';
+                                            echo $getMembers->surname_prefix.' ';
+                                            echo $getMembers->surname.'</p><br>';
+                                        }
                                     }
                                 }
-                            }
-                        echo '</p>
+                            echo '
+                            </div>
                         </div>
                     ';
                     }
