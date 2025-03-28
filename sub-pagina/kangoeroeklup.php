fffc7eaa97c37221c63dc333334d685e13b05c2b
Martijn Posthuma <martijn.posthuma@eijsink.nl>
2025-03-28 11:39:13 +0100

    Add missing dot

--- sub-pagina/kangoeroeklup.php
+++ sub-pagina/kangoeroeklup.php
@@ -11,7 +11,7 @@
         <div class="col-md-12 col-xs-12">
             <h1>De ‘Kangoeroe Klup’ gaat weer van start!<h1/><hr>
                 <img src="../images/Kangoeroeklup.png" alt="Kangoeroe klup" style="margin: 10px; float: right;" class="img-responsive"/>
-                <h3>Kijk voor de data's en laatste informatie onder het kopje 'Laatste nieuws'</h3>
+                <h3>Kijk voor de data's en laatste informatie onder het kopje 'Laatste nieuws'.</h3>
                 <p>Op het korfbalveld te sportpark Zwanenveld (Zwanenveld 5) in de wijk Rietlanden.<br><br>
                     De Kangoeroe Klup is een gezellige club voor kinderen van 3 tot 7 jaar waarbij je lekker kunt sporten met leeftijdsgenootjes. Bij de korfbalvereniging EKC 2000 bereiden elke week speciaal hiervoor opgeleide leiders een training voor.<br><br>
                     De naam ‘Kangoeroe Klup’ is door het korfbalverbond (KNKV) gekozen daar de kangoeroe veel overeenkomsten heeft met vaardigheden die horen bij de korfbalsport, zoals snelheid, wendbaarheid en sprongkracht. Daarnaast heeft de kangoeroe een buidel die aan jonge kangoeroes bescherming en veiligheid biedt! Om de Kangoeroe Klup sprekend te maken, is door het KNKV een viertal jeugdige kangoeroes ontwikkeld. Deze figuurtjes, genaamd Scoro, Jumper, Funny en Spurt , vervullen een hoofdrol binnen de Kangoeroe Klup.
